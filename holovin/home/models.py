fb729d53d1ae0ee618d004d755ecc9e02fecdad0
Viktor Holovin <holovin@mail.com>
2020-04-01 20:44:23 +0200

    add image models to carousel

--- holovin/home/models.py
+++ holovin/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class HomeImage(models.Model):
+    name = models.CharField(max_length=200)
+    image_url = models.URLField(max_length=200, blank=True, null=True)
+    image_file = models.FileField(upload_to='homeimages/', blank=True, null=True)
+    active = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
--- holovin/home/views.py
+++ holovin/home/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-
+from .models import HomeImage
 
 def home(request):
-    return render(request, 'home/home.html',)
+    images = HomeImage.objects.all()
+    return render(request, 'home/home.html', {'images': images})
